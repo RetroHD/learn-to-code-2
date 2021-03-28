d2b59effe014361ba39b1b96dcb6b7c7a38f2239
Clayton Jensen <cj.krysk+github@protonmail.com>
2021-03-28 12:38:49 -0500

    add page placeholders

---
+++ pages/about.jsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, autem
+      pariatur similique, est eius ipsam mollitia asperiores suscipit, provident
+      dolore deserunt. Hic nostrum dolores ipsa. Fugiat alias quod
+      necessitatibus inventore!
+    </h1>
+  );
+}
