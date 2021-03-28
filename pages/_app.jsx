219520cff8bff0c8291bcec9adcff01a2952a5a9
Clayton Jensen <cj.krysk+github@protonmail.com>
2021-03-28 13:03:58 -0500

    very bad theme changer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import ThemeButton from "../components/ThemeButton";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isDark, setDark] = useState(false);
+
+  return (
+    <>
+      <ThemeButton setDark={setDark} isDark={isDark} />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          background-color: ${isDark ? "#000" : "#fff"};
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
